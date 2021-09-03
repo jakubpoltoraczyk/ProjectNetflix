4ced5439ee89843c44defd1dc809ab29f9b64d8a
Filip Poltoraczyk <f.poltoraczyk@gmail.com>
2021-09-03 19:36:55 +0200

    Updated

--- srcback/person/personalinformation.py
+++ srcback/person/personalinformation.py
@@ -1,13 +1,22 @@
-
 import datetime
 from dateutils import DateUtils
 from date import Date
+
 now = datetime.datetime.now()
 
 class Person(object):
     """Class which give us the most important information about a person"""
 
-    def __init__(self, name, surname, nationality, date_of_birth, gender, photo_link, date_of_death):
+    def __init__(
+        self,
+        name,
+        surname,
+        nationality,
+        date_of_birth,
+        gender,
+        photo_link,
+        date_of_death,
+    ):
         """Contains basic information about this person
 
         Params:
@@ -18,15 +27,20 @@ class Person(object):
             gender : the gender of this person
             photo_link_link : url link image of this person
         """
-        self.__name = self.__surname = self.__nationality = self.__date_of_birth  = self.__date_of_death = self.__gender = self.__photo_link = None
-        self.set_date_of_birth( date_of_birth, date_of_death)
-        self.set_date_of_death( date_of_birth, date_of_death)
+        self.__name = (
+            self.__surname
+        ) = (
+            self.__nationality
+        ) = (
+            self.__date_of_birth
+        ) = self.__date_of_death = self.__gender = self.__photo_link = None
         self.set_surname(surname)
         self.set_nationality(nationality)
-        self.set_date_of_birth( date_of_birth, date_of_death)
-        self.set_date_of_death( date_of_birth, date_of_death)
+        self.set_date_of_birth(date_of_birth, date_of_death)
+        self.set_date_of_death(date_of_birth, date_of_death)
         self.set_gender(gender)
         self.set_photo_link(photo_link)
+
     def get_name(self):
         """Provide name
 
@@ -56,21 +70,20 @@ class Person(object):
             the nationality of this person"""
         return self.__nationality
 
-    def get_date_of_birth (self):
+    def get_date_of_birth(self):
         """Provide age
 
         Returns:
             the date of birth of this person"""
-        return self.__date_of_birth 
-    def get_date_of_death (self, date_of_death):
+        return self.__date_of_birth
+
+    def get_date_of_death(self, date_of_death):
         """Provide age
 
         Returns:
             the date of death of this person, if he is still alive, prints still alive"""
-        if date_of_death.get_day() == now.day():
-            if date_of_death.get_month() == now.month():
-                if date_of_death.get_year() == now.year():
-                    return "still alive"
+        if DateUtils.are_equal(date_of_death, Date(now.day, now.month, now.year)):
+            print("still alive")
         else:
             return self.__date_of_death
 
@@ -111,20 +124,20 @@ class Person(object):
             nationality : new nationality which will be set"""
         self.__nationality = nationality.capitalize()
 
-    def set_date_of_birth (self, date_of_birth, date_of_death):
+    def set_date_of_birth(self, date_of_birth, date_of_death):
         """Change the date of birth
         Args:
             date_of_birth  : new date of birth  which will be set"""
         if self.__validate_date_of_birth_and_death(date_of_death, date_of_birth):
-            self.__date_of_birth  = date_of_birth 
+            self.__date_of_birth = date_of_birth
 
-    def set_date_of_death(self, date_of_death, date_of_birth ):
+    def set_date_of_death(self, date_of_death, date_of_birth):
         """Change the date of death
 
         Args:
             date_of_death  : new date of death  which will be set"""
         if self.__validate_date_of_birth_and_death(date_of_death, date_of_birth):
-            self.__date_of_death  = date_of_death
+            self.__date_of_death = date_of_death
 
     def set_gender(self, gender):
         """Change the gender, only allows to use strings : "male", "female"
@@ -158,15 +171,15 @@ class Person(object):
         if surname != "":
             return True
         return False
+
     def __validate_date_of_birth_and_death(self, date_of_death, date_of_birth):
         """Check if date of birth was before date of death(if it was)
-        
+
         Returns:
-            True if date of birth was before date of death, otherwise false """
+            True if date of birth was before date of death, otherwise false"""
         if DateUtils.get_younger(date_of_death, date_of_birth) == date_of_birth:
             return True
         return False
-        
 
     def __validate_gender(self, gender):
         """Check if gender is male or female
@@ -191,7 +204,7 @@ def add_person(people):
     month = int(input("Write month of birth: "))
     year = int(input("Write year of birth: "))
     date_of_birth = Date(day, month, year)
-    date_of_death = input("Are they dead? Answer using 'yes' or 'no'" )
+    date_of_death = input("Are they dead? Answer using 'yes' or 'no'")
     if date_of_death == "yes":
         day = int(input("Write day of death: "))
         month = int(input("Write month of death: "))
@@ -205,4 +218,8 @@ def add_person(people):
     while gender not in ["male", "female"]:
         gender = input("Write the gender of the person: ")
     photo_link = input("Add photo_link link of the person: ")
-    people.append(Person( name, surname, nationality, date_of_birth, gender, photo_link, date_of_death))
+    people.append(
+        Person(
+            name, surname, nationality, date_of_birth, gender, photo_link, date_of_death
+        )
+    )
