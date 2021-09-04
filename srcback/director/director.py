4d8ba52367ae776c63a0b50fb7e5974766a424d5
Filip Poltoraczyk <f.poltoraczyk@gmail.com>
2021-09-04 14:04:59 +0200

    updated

--- srcback/director/director.py
+++ srcback/director/director.py
@@ -21,10 +21,11 @@ class Director(Person):
         photo_link,
         rating,
         efficienty,
-        date_of_death = Date(now.day, now.month, now.year),
-        movie=[],
+        number_of_movies,
+        date_of_death=Date(now.day, now.month, now.year),
     ):
         """Contains basic information about actor
+
         Params:
             name : the name of this person writing capital letter
             surname: the surname of person writing capital letter
@@ -35,34 +36,39 @@ class Director(Person):
             rating(float): rating of acting
             efficienty(float): the average value of directed films in one year
             date_of_death:  the date of death of this person
-            movie: List of movies
         """
-        super().__init__(name, surname, nationality, date_of_birth, gender, photo_link, date_of_death)
+        super().__init__(
+            name, surname, nationality, date_of_birth, gender, photo_link, date_of_death
+        )
         rating = efficienty = None
         self.set_rating(rating)
         self.set_efficienty(efficienty)
-        self.set_movie(movie)
+        self.set_number_of_movies(number_of_movies)
 
     def get_rating(self):
         """Provide rating of director
+
         Returns:
             Rating of director as a number from 0 to 10"""
         return self.__rating
 
     def get_efficienty(self):
         """Provide value of efficienty
+
         Returns:
             The average value of directed films in one year"""
         return self.__efficienty
 
-    def get_movie(self):
-        """Provide movies directed by this director
-        Returns:
-            List of movies"""
-        return self.__movie
+    def get_number_of_movies(self):
+        """Provide number of movies directed by director
 
+        Returns:
+            Number of movies directed by director"""
+        return self.__number_of_movies
+        
     def set_rating(self, rating):
         """Set rating of the acting
+
         Args:
             rating(float): New rating as a number from 0 to 10"""
         if 0 <= rating <= 10:
@@ -72,15 +78,14 @@ class Director(Person):
 
     def set_efficienty(self, efficienty):
         """Set value of efficienty
+
         Args:
             efficienty(float): New value of efficienty"""
-        efficienty = float(len(self.get_movie()) / super().get_age())
+        efficienty = float(self.__number_of_movies / super().get_age())
 
-    def set_movie(self, movie):
-        """Set movies directed by this director
+    def set_number_of_movies(self, number_of_movies):
+        """Set new number of movies which this director has directed
+
         Args:
-            movie: List of movies"""
-        for x in movies:
-            print(x + 1, "-", movies[x].get_title())
-        click = int(input("Which one did this director direct? "))
-        movie.append(movies[x])
+            movies(int): New value od movies"""
+        self.__number_of_movies = number_of_movies
